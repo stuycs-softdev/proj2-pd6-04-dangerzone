a9b0de0b6b53b0b75a8810c8e4f58c22e314657d
Aaron Coppa <aacoppa@gmail.com>
2013-11-16 14:59:16 -0500

    Foundations for the project
    Added app.py with different pages. Started Utils.py for topic
    confirmation. Also added templates and static directories

---
+++ utils.py
@@ -0,0 +1,6 @@
+def cleanUpTopic(topic):
+    topic = topic.lower()
+    #Check for white-space and deal with that
+    #also do any other clean up neccesary (maybe confirm that this topic is possible)
+    return topic
+
