3649c73133efdf2d2921525cdf9bcd4072c865ee
Jing Lin <Jinglin82@gmail.com>
2013-11-20 12:18:50 -0500

    "NYT"

--- API/APIs.py
+++ API/APIs.py
@@ -5,16 +5,18 @@ NYT_API_URL = 'http://api.nytimes.com/svc/search/v2/articlesearch'
 
 def FindArticles(Topic):
     API_KEY = '5772CD9A42F195C96DA0E930A7182688:14:68439177'
-    format = ".json"
+    FORMAT = ".json"
     Q = str(Topic)
     FQ = "The New York Times"
     BEGIN_DATE = 19000101
     FACET_FIELD = "section_name"
-    FACET_FILTER = true
-    HL = true
+    FACET_FILTER = 1
+    HL = 1
 
-    url = ('http://api.nytimes.com/svc/search/v2/articlesearch%s?q=%s&fq=%s&facet_field=&%s&facet_filter=%s&begin_date=%d&hl=%s&api-key=%s') % (Q, FQ, FACET_FIELD, FACET_FILTER, BEGIN_DATE, HL, API_KEY)
+    url = ('http://api.nytimes.com/svc/search/v2/articlesearch%s?q=%s&fq=%s&facet_field=&%s&facet_filter=%s&begin_date=%d&hl=%s&api-key=%s') % (FORMAT, Q, FQ, FACET_FIELD, FACET_FILTER, BEGIN_DATE, HL, API_KEY)
     reponse = urlopen(url)
     Json_Data = json.loads(response.read())
+    return Json_Data
 
-
+if __name__ == '__main__':
+    FindArticles("Apples")
