ac83cbde4fe1b9a50f558ce1f70164390136c687
Ben Kurtovic <ben.kurtovic@gmail.com>
2013-11-22 12:29:18 -0500

    Fairly complete lient/server document updating.

--- omnithinker/connection.py
+++ omnithinker/connection.py
@@ -1,3 +1,4 @@
+from json import dumps, loads
 from logging import getLogger
 
 from .document import Document
@@ -32,6 +33,11 @@ class Connection(object):
         self._log("SEND", data)
         self._socket.send(data)
 
+    def _error(self):
+        """Client has sent bad data; close the connection with an error."""
+        self._state = STATE_CLOSING
+        self._send(SVERB_INVALID, REPLY_INVALID)
+
     def _handle_state_waiting(self, verb, data):
         """Handle input from the client when in the "waiting" state."""
         if verb == CVERB_OPEN:
@@ -39,20 +45,26 @@ class Connection(object):
             self._document = Document()
             self._send(SVERB_READY)
         else:
-            self._state = STATE_CLOSING
-            self._send(SVERB_INVALID, REPLY_INVALID)
+            self._error()
 
     def _handle_state_ready(self, verb, data):
         """Handle input from the client when in the "ready" state."""
         if verb == CVERB_UPDATE:
-            ### process update ###
+            try:
+                data = loads(data)
+            except ValueError:
+                self._error()
+                return
+            if "title" in data:
+                self._document.title = data["title"]
+            if "text" in data:
+                self._document.text = data["text"]
             self._send(SVERB_OK)
         elif verb == CVERB_CLOSE:
             self._state = STATE_CLOSING
             self._send(SVERB_BYE)
         else:
-            self._state = STATE_CLOSING
-            self._send(SVERB_INVALID, REPLY_INVALID)
+            self._error()
 
     def handle(self):
         """Handle the main server/client connection loop."""
@@ -64,10 +76,12 @@ class Connection(object):
             data = data.strip()
             self._log("RECV", data)
             if not data:
-                self._state = STATE_CLOSING
-                self._send(SVERB_INVALID, REPLY_INVALID)
+                self._error()
                 break
-            verb = data.split()[0]
+            try:
+                verb, data = data.split(" ", 1)
+            except ValueError:
+                verb, data = data, None
             if self._state == STATE_WAITING:
                 self._handle_state_waiting(verb, data)
             elif self._state == STATE_READY:
--- static/omnithinker.js
+++ static/omnithinker.js
@@ -1,6 +1,9 @@
 var URL = "ws://localhost:8000/socket";
 var output;
+var textbox;
 var socket;
+var timeout_id = 0;
+var last_text_hash = "";
 
 function display(message) {
     var p = document.createElement("p");
@@ -13,8 +16,19 @@ function send(message) {
     socket.send(message);
 }
 
+function update_server() {
+    var data = {"text": textbox.innerHTML};
+    send("UPDATE " + JSON.stringify(data));
+}
+
+function gen_hash(s) {
+    // http://stackoverflow.com/questions/7616461
+    return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
+}
+
 function init() {
     output = document.getElementById("output");
+    textbox = document.getElementById("textbox");
     socket = new WebSocket(URL);
 
     socket.onopen = function(event) {
@@ -52,6 +66,15 @@ function init() {
 
     $("#disconnect").click(function() {
         send("BYE");
+    });
+
+    $("#textbox").keyup(function(e) {
+        var this_hash = gen_hash(textbox.innerHTML);
+        if (this_hash != last_text_hash) {
+            last_text_hash = this_hash;
+            window.clearTimeout(timeout_id);
+            timeout_id = window.setTimeout(update_server, 500);
+        }
     });
 }
 
