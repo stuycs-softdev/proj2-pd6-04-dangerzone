48560e0eb09e57913612a2fd08d84864338b670b
Jing Lin <Jinglin82@gmail.com>
2013-11-29 13:07:02 -0500

    "API NYTIMES."

--- omnithinker/api_nytimes.py
+++ omnithinker/api_nytimes.py
@@ -1,28 +1,35 @@
+#!/usr/bin/python
+
 import json
-from urllib2 import urlopen
-from collections import defaultdict
+from urllib import urlopen
 
 NYT_API_URL = 'http://api.nytimes.com/svc/search/v2/articlesearch'
 
 def FindArticles(Topic):
-    API_KEY = '5772CD9A42F195C96DA0E930A7182688:14:68439177'
-    FORMAT = ".json"
-    Q = str(Topic)
-    FQ = "The New York Times"
-    BEGIN_DATE = 19000101
-    FACET_FIELD = "section_name"
-    FACET_FILTER = 1
-    HL = 1
+    API_KEY = "5772CD9A42F195C96DA0E930A7182688:14:68439177"
+    FORMAT = "json"
+    FQ = str(Topic)
+    FACET_FIELD = "day_of_week"
+    BEGIN_DATE = str(19000101)
 
-    url = ('http://api.nytimes.com/svc/search/v2/articlesearch%s?q=%s&fq=%s&facet_field=&%s&facet_filter=%s&begin_date=%d&hl=%s&api-key=%s') % (FORMAT, Q, FQ, FACET_FIELD, FACET_FILTER, BEGIN_DATE, HL, API_KEY)
-    reponse = urlopen(url)
+    url = ("http://api.nytimes.com/svc/search/v2/articlesearch.%s?fq=%s&FACET_FIELD=%s&BEGIN_DATE=%s&API-KEY=%s") % (FORMAT, FQ, FACET_FIELD, BEGIN_DATE, API_KEY)
+    response = urlopen(url)
     Json_Data = json.loads(response.read())
+    print url
+    print Json_Data
 
-    NYT_Filter = {'Title' : ' ', 'Text' : ' ', 'URL' : ' ', 'Topics' : ' '}
-    for x in docs:
-        NYT_Filter['Title'] = NYT_Filter['Title'] + ' ' + Json_Data['docs']['headline'][0]
-        NYT_Filter['Text'] = Json_Data['docs']['lead_paragraph']
+    URL = list()
+    TITLE = list()
+    Snippet = list()
+    Counter = 0
+    # for x in Json_Data["response"]["Docs"]:
+    #     if x == "web_url":
+    #         Counter = Counter + 1
+    #         URL.append(Json_Data["response"]["Docs"][x])
+    #         print("Hello!")
+
+
     return Json_Data
 
 if __name__ == '__main__':
-    FindArticles("Apples")
+    FindArticles("Obama")
